9753856aee416002392e0d53d8b23841824e95ac
Paolo Rechia <paolorechia@gmail.com>
2026-09-24 14:01:04 -0300

    Refactor to add a ConnectionManager abstraction

--- src/connection_manager.rs
+++ src/connection_manager.rs
@@ -1,33 +1,45 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
-pub async fn init() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        println!("Listening on 127.0.0.1:8080");
-        tokio::spawn(async move {
-            println!("Spawning... {:?}", socket);
-            let mut buf = [0; 64];
+pub struct ConnectionManager {
+    listener: TcpListener,
+    endpoint: String,
+}
 
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(0) => {
-                        println!("Connection closed: {:?}", socket);
-                        return;
-                    },
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("Failed to read from socket; err = {:?}", e);
+impl ConnectionManager {
+    pub async fn init(endpoint: String) -> Result<ConnectionManager, Box<dyn std::error::Error>> {
+        return Ok(ConnectionManager {
+            listener: TcpListener::bind(&endpoint).await?,
+            endpoint: endpoint,
+        });
+    }
+    pub async fn serve(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        loop {
+            let (mut socket, _) = self.listener.accept().await?;
+            println!("Listening on {}", self.endpoint);
+            tokio::spawn(async move {
+                println!("Spawning... {:?}", socket);
+                let mut buf = [0; 64];
+
+                loop {
+                    let n = match socket.read(&mut buf).await {
+                        Ok(0) => {
+                            println!("Connection closed: {:?}", socket);
+                            return;
+                        }
+                        Ok(n) => n,
+                        Err(e) => {
+                            eprintln!("Failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+                    println!("Got: {}", n);
+                    if let Err(e) = socket.write_all(&buf[0..n]).await {
+                        eprintln!("Failed to write socket; err = {:?}", e);
                         return;
                     }
-                };
-                println!("Got: {}", n);
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("Failed to write socket; err = {:?}", e);
-                    return;
                 }
-            }
-        });
+            });
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-
 pub mod connection_manager;
 
-use crate::connection_manager::init;
+use crate::connection_manager::ConnectionManager;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    return init().await;
+    let mut connection_manager = ConnectionManager::init("127.0.0.1:8080".to_string()).await;
+    match connection_manager {
+        Ok(mut connection_manager) => connection_manager.serve().await,
+        Err(err) => Err(err),
+    }
 }
