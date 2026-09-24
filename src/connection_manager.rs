89e40189e4d87aa8cfa08d74af9f6400b78601e1
Paolo Rechia <paolorechia@gmail.com>
2026-09-24 11:23:34 -0300

    Small refactor, clean main

---
+++ src/connection_manager.rs
@@ -0,0 +1,33 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+pub async fn init() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        println!("Listening on 127.0.0.1:8080");
+        tokio::spawn(async move {
+            println!("Spawning... {:?}", socket);
+            let mut buf = [0; 64];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(0) => {
+                        println!("Connection closed: {:?}", socket);
+                        return;
+                    },
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+                println!("Got: {}", n);
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("Failed to write socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
 
-pub mod hello;
+pub mod connection_manager;
 
-use crate::hello::print_hello;
+use crate::connection_manager::init;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        println!("Listening on 127.0.0.1:8080");
-        tokio::spawn(async move {
-            println!("Spawning...");
-            let mut buf = [0; 64];
-
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    Ok(0) => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("Failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-                println!("Got: {}", n);
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("Failed to write socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }
+    return init().await;
 }
